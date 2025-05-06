ed53d0025ef4a64682168ae527777ce4d12841c1
HarshYadav0508 <heyitsharshyadav@gmail.com>
2025-05-06 18:49:36 +0530

    Set up Theme.jsx

---
+++ frontend/portolio/src/components/Theme/Theme.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './theme.css';
+
+const Theme = () => {
+  return (
+    <div className='background-wrapper'>
+        <h1 className='blur-overlay'>Welcome, what is your type?</h1>
+    </div>
+  )
+}
+
+export default Theme
+
+
